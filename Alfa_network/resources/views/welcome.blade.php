74cb341f603df7eefd775d421b98b77041c7c181
CheeseDude01 <alfarf04@gmail.com>
2025-09-03 22:50:32 +0700

    First push

---
+++ Alfa_network/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alfa's Network</title>
+</head>
+<body>
+    <h1>welcome to my (Alfa) Network</h1>
+    <p>Click the button below to see my GitHub repo</p>
+
+    <a href="" class="">
+        GitHub
+    </a>
+</body>
+</html>
