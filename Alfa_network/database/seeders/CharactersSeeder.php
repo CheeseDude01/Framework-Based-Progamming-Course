d59f99f223dcc14d61286e8b81d23bc8955d84e7
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-16 22:03:44 +0700

    Update CharactersSeeder.php

--- Alfa_network/database/seeders/CharactersSeeder.php
+++ Alfa_network/database/seeders/CharactersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Characters;
 
 class CharactersSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class CharactersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Characters::factory()->count(5)->create();
     }
 }
