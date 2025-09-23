876ba1259d2bae3762664e7d0304e152b0549ff8
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-23 17:43:43 +0700

    Update show.blade.php

--- Alfa_network/resources/views/character/show.blade.php
+++ Alfa_network/resources/views/character/show.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
-    <h2>   {{ $character->name }}</h2>
+    <h2>   {{ $characters->name }}</h2>
 
     <div class="bg-gray-200 p-4 rounded">
-        <p><strong>Skill level:</strong> {{ $character->skill}}</p>
+        <p><strong>Skill level:</strong> {{ $characters->skill}}</p>
         <p><strong>About me:</strong></p>
-        <p>{{ $character->bio }}</p>
+        <p>{{ $characters->bio }}</p>
     </div>
     </x-layout>
