40f151f4432f0532b1644921a9f75ee771d96f28
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-23 15:45:52 +0700

    Update index.blade.php

--- Alfa_network/resources/views/character/index.blade.php
+++ Alfa_network/resources/views/character/index.blade.php
@@ -2,10 +2,10 @@
     <h2>Available Character</h2>
 
     <ul>
-        @foreach ($characters as $character)
+        @foreach ($character as $character)
             <li>
-                <x-card href="/character/{{ $character['id'] }}" :highlight="$character['skill'] > 70">
-                    <h3>{{ $character['name']}}</h3>
+                <x-card href="/character/{{ $character->id }}" :highlight="$character['skill'] > 70">
+                    <h3>{{ $character->name}}</h3>
                 </x-card>
             </li>
         @endforeach
