9d74df800060c6ce288e96f3184be39a45dbff4e
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-23 15:46:48 +0700

    Update CharacterController.php

--- Alfa_network/app/Http/Controllers/CharacterController.php
+++ Alfa_network/app/Http/Controllers/CharacterController.php
@@ -9,20 +9,31 @@ class CharacterController extends Controller
 {
     public function index(){
         // route---> /character/
-        $characters = Characters::orderBy('created_at', 'desc')->get();
+        $character = Characters::orderBy('created_at', 'desc')->get();
 
-        return view('character.index', ["characters" => $characters]);
+        return view('character.index', ["character" => $character]);
     }
 
     public function show($id){
+        // route --> /character/{id}
+        $character = Characters::findOrFail($id);
 
+        return view('character.show', ["character" => $character]);
     }
 
     public function create(){
+        // route --> /character/create
+        // render a create view (with web form) to users
 
+        return view('character.create');
     }
 
     public function store(){
 
+    }
+
+    public function destroy($id){
+        // --> /character/{id} (DELETE)
+        // handle delete request to delete a ninja record from table
     }
 }
