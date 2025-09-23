9c08fb129777fb567c87af25e0ce99840e529390
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-23 15:46:08 +0700

    Update show.blade.php

--- Alfa_network/resources/views/character/show.blade.php
+++ Alfa_network/resources/views/character/show.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-    <h2>
-        Character id - {{ $id }}
-    </h2>
-</x-layout>
+    <h2>   {{ $character->name }}</h2>
+
+    <div class="bg-gray-200 p-4 rounded">
+        <p><strong>Skill level:</strong> {{ $character->skill}}</p>
+        <p><strong>About me:</strong></p>
+        <p>{{ $character->bio }}</p>
+    </div>
+    </x-layout>
