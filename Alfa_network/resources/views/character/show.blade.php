bfc5219dd0ee4e988314f5c26fb0924838fff496
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-30 20:36:14 +0700

    Update show.blade.php

--- Alfa_network/resources/views/character/show.blade.php
+++ Alfa_network/resources/views/character/show.blade.php
@@ -6,4 +6,12 @@
         <p><strong>About me:</strong></p>
         <p>{{ $characters->bio }}</p>
     </div>
+
+    <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
+        <h3>Faction Information</h3>
+        <p><strong>Faction name:</strong> {{ $characters->faction->name }}</p>
+        <p><strong>Location:</strong> {{ $characters->faction->location }}</p>
+        <p><strong>About the Faction:</strong></p>
+        <p>{{ $characters->faction->description }}</p>
+    </div>
     </x-layout>
