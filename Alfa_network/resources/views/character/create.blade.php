e56c3893f4a8c6de39f9d4bcaf66bedda3a47e84
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-30 21:21:52 +0700

    Update create.blade.php

--- Alfa_network/resources/views/character/create.blade.php
+++ Alfa_network/resources/views/character/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  <form action="" method="">
+  <form action="{{ route('character.store') }}" method="POST">
     <!-- CSRF token for security -->
     @csrf
 
@@ -11,6 +11,7 @@
       type="text" 
       id="name" 
       name="name" 
+      value="{{ old('name') }}" 
       required
     >
 
@@ -20,6 +21,7 @@
       type="number" 
       id="skill" 
       name="skill" 
+      value="{{ old('skill') }}" 
       required
     >
 
@@ -30,14 +32,14 @@
       id="bio" 
       name="bio" 
       required
-    ></textarea>
+    >{{ old('bio') }}</textarea>
 
     <!-- select a dojo -->
     <label for="faction_id">Faction:</label>
     <select id="faction_id" name="faction_id" required>
       <option value="" disabled selected>Select a faction</option>
       @foreach ($factions as $faction)
-        <option value="{{ $faction->id }}">
+        <option value="{{ $faction->id }}" {{ $faction->id == old('faction_id') ? 'selected' : '' }}>
           {{ $faction->name }}
         </option>
       @endforeach
@@ -46,6 +48,13 @@
     <button type="submit" class="btn mt-4">Create Character</button>
 
     <!-- validation errors -->
+    @if ($errors->any())
+        <ul class="px-4 py-2 bg-red-200">
+            @foreach ($errors->all() as $error)
+                <li class="my-2 text-red-600">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     
   </form>
 </x-layout>
