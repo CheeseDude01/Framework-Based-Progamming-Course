0ac478bc666987654f0d3754d749dcf5fa328ca0
Alfa <113441135+CheeseDude01@users.noreply.github.com>
2025-09-30 20:34:28 +0700

    Add files via upload

---
+++ Alfa_network/database/migrations/2025_09_16_000002_create_characters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('skill');
+            $table->text('bio');
+            $table->foreignId('faction_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
